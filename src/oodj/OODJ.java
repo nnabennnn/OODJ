abe8a50444bf2cb87e4c189db76015d54d779db8
Htan Khai Shan <htankhaishan@Htans-MacBook-Pro.local>
2023-07-30 15:51:04 +0800

    bashTest2

--- src/oodj/OODJ.java
+++ src/oodj/OODJ.java
@@ -15,7 +15,7 @@ public class OODJ {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("");
+        System.out.println(" ");
     }
     
 }
